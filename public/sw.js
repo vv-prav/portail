b2b8e23a853c4d61a477544a3b321598cac8b53c
vv-prav <victor.verite@gmail.com>
2026-07-20 15:06:13 +0200

    Add files via upload

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,5 @@
 // Incrémente cette version à CHAQUE changement de fichier statique.
-const CACHE_VERSION = 'salon-v8';
+const CACHE_VERSION = 'salon-v9';
 const CORE = [
     '/', '/index.html', '/app.js', '/style.css', '/manifest.json',
     '/icon-192.png', '/icon-512.png',
